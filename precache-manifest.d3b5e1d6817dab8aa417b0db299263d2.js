e7e48cf0dc4519722cd74c7282c23af19637b583
zak258 <zakaria.k.hamdan@gmail.com>
2020-06-24 13:30:26 +0200

    Updates

---
+++ precache-manifest.d3b5e1d6817dab8aa417b0db299263d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "614b16c3d290d9ba5247d96f5bc7473b",
+    "url": "/ZakariaHn/online-shop-react/index.html"
+  },
+  {
+    "revision": "9ea1d8e25e40964fa691",
+    "url": "/ZakariaHn/online-shop-react/static/css/main.27c1270b.chunk.css"
+  },
+  {
+    "revision": "e834ef4880a23006ee74",
+    "url": "/ZakariaHn/online-shop-react/static/js/2.736166c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ZakariaHn/online-shop-react/static/js/2.736166c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ea1d8e25e40964fa691",
+    "url": "/ZakariaHn/online-shop-react/static/js/main.e95c49be.chunk.js"
+  },
+  {
+    "revision": "fad2aa2f4408479f53ac",
+    "url": "/ZakariaHn/online-shop-react/static/js/runtime-main.c417cedf.js"
+  }
+]);
