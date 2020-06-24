41e043875986a9b0fff511b76ccc955727dd122c
zak258 <zakaria.k.hamdan@gmail.com>
2020-06-24 15:12:17 +0200

    total price for each item

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -5,6 +5,8 @@ export default class ProductItem extends React.Component {
     super(props);
     this.state = {
       ammount: 0,
+      totalPrice: 0,
+      total: [],
       data: this.props.info,
     };
   }
@@ -12,27 +14,31 @@ export default class ProductItem extends React.Component {
   increment = () => {
     this.setState({
       ammount: this.state.ammount + 1,
+      totalPrice:
+        this.state.ammount * this.state.data.price + this.state.data.price,
     });
+    let totalAmount = 0;
+    totalAmount += this.state.ammount;
   };
+
   decrement = () => {
     this.setState({ ammount: this.state.ammount - 1 });
   };
 
   render() {
+    const { id, price, productName, icon, inventory } = this.state.data;
     return (
-      <li key={this.state.data.id}>
-        {this.state.data.productName} <i>{this.state.data.icon}</i>
-        <h5>{this.state.data.price} Eu</h5>
-        <button
-          disabled={this.state.data.inventory === 0}
-          onClick={this.increment}
-        >
-          {this.state.data.inventory > 0 ? "Add to cart" : "Sold out"}
+      <li key={id}>
+        {productName} <i>{icon}</i>
+        <h5>{price} Eu</h5>
+        <button disabled={inventory === 0} onClick={this.increment}>
+          {inventory > 0 ? "Add to cart" : "Sold out"}
         </button>
         <button disabled={this.state.ammount === 0} onClick={this.decrement}>
           Remove from cart
         </button>
         <h2>{this.state.ammount}</h2>
+        <h2>TotalPrice {this.state.totalPrice}</h2>
       </li>
     );
   }
